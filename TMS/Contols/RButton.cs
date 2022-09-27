55d36e2a9c2b28d0f3e9ffbded571531f4df1007
Keith <Keith@DESKTOP-S30EBAT>
2022-09-26 21:57:19 -0500

    Add project files.

---
+++ TMS/Contols/RButton.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.ComponentModel;
+
+namespace Tool_Inventory_System
+{
+    public class RButtons : Button
+    {
+        //Variables
+        private int borderSize = 0;
+        private int borderRadius = 40;
+        private Color borderColor = Color.PaleVioletRed;
+
+        //Button Properties
+        [Category("RButton Properties")]
+        public int BorderSize
+        {
+            get
+            {
+                return borderSize;
+            }
+            set
+            {
+                borderSize = value;
+                Invalidate();
+            }
+        }
+
+        [Category("RButton Properties")]
+        public int BorderRadius
+        {
+            get
+            {
+                return borderRadius;
+            }
+            set
+            {
+                //Ensure border is less then Height
+                if (value <= Height)
+                    borderRadius = value;
+                else borderRadius = Height;
+
+                Invalidate();
+            }
+        }
+
+        [Category("RButton Properties")]
+        public Color BorderColor
+        {
+            get
+            {
+                return borderColor;
+            }
+            set
+            {
+                borderColor = value;
+                Invalidate();
+            }
+        }
+
+        [Category("RButton Properties")]
+        public Color BackgroundColor
+        {
+            get { return BackColor; }
+            set { BackColor = value; }
+        }
+
+        [Category("RButton Properties")]
+        public Color TextColor
+        {
+            get { return ForeColor; }
+            set { ForeColor = value; }
+        }
+
+        //Constructor
+        public RButtons()
+        {
+            FlatStyle = FlatStyle.Flat;
+            FlatAppearance.BorderSize = 0;
+            Size = new Size(150, 40);
+            BackColor = Color.MediumSlateBlue;
+            ForeColor = Color.White;
+            Resize += new EventHandler(Button_Resize);
+        }
+
+        //Methods
+        private GraphicsPath GetFigurePath(RectangleF rect, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            path.AddArc(rect.Width - radius, rect.Y, radius, radius, 270, 90);
+            path.AddArc(rect.Width - radius, rect.Height - radius, radius, radius, 0, 90);
+            path.AddArc(rect.X, rect.Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+
+            return path;
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            RectangleF rectSurface = new RectangleF(0, 0, Width, Height);
+            RectangleF rectBorder = new RectangleF(1, 1, Width - 0.8F, Height - 1);
+
+            //Rounded Button
+            if (borderRadius > 2)
+            {
+                using (GraphicsPath pathSurface = GetFigurePath(rectSurface, borderRadius))
+                using (GraphicsPath pathBorder = GetFigurePath(rectBorder, borderRadius - 1F))
+                using (Pen penSurface = new Pen(Parent.BackColor, 2))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                {
+                    penBorder.Alignment = PenAlignment.Inset;
+
+                    //Button Surface
+                    Region = new Region(pathSurface);
+
+                    //Draw Surface Border for HD Result
+                    pevent.Graphics.DrawPath(penSurface, pathSurface);
+
+                    //Draw Control Border
+                    if (borderSize >= 1)
+                        pevent.Graphics.DrawPath(penBorder, pathBorder);
+                }
+
+            }
+            //Normal Button
+            else
+            {
+                Region = new Region(rectSurface);
+                if (borderSize >= 1) ;
+                {
+                    using (Pen penBorder = new Pen(borderColor, borderSize))
+                    {
+                        penBorder.Alignment = PenAlignment.Inset;
+                        pevent.Graphics.DrawRectangle(penBorder, 0, 0, Width - 1, Height - 1);
+
+                    }
+                }
+
+            }
+        }
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            Parent.BackColorChanged += new EventHandler(Container_BackColorChanged);
+        }
+
+        //Background Color Changed
+        private void Container_BackColorChanged(object sender, EventArgs e)
+        {
+            if (DesignMode)
+                Invalidate();
+        }
+
+        //Button Resize Event
+        private void Button_Resize(object sender, EventArgs e)
+        {
+            if (borderRadius > Height)
+                BorderRadius = Height;
+
+        }
+    }
+}
